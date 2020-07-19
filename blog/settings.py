139121d60e5baeca2bc5dd81a762b08613b5607b
Praveen Kumar <pkumar22@jcp.com>
2020-07-19 15:30:35 +0530

    'first-commit'

--- blog/settings.py
+++ blog/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog1',
-    'subscribe',
     'form',
     'home',
     'post',
--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('subscribe/',include('subscribe.urls')),
     path('register/',include('form.urls')),
     path('upload/',include('profile_maker.urls')),
     path('',include('home.urls')),
